73ba8b84b6a54f3f136695be52ce8701aacb7563
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2026-03-31 22:29:31 -0300

    Create Atv.py

---
+++ Atv.py
@@ -0,0 +1,158 @@
+#Luiz Felipe Da Silva
+
+#Nivel 1
+#atv 1
+
+# idade = int(input('Digite sua idade: '))
+
+# if idade >= 18:
+#     print("Você é maior de idade")
+# else: 
+#     print('Você é menor de idade')
+    
+######
+
+#atv 2
+
+# numero = float(input("Digite um numero: "))
+
+# if numero > 0:
+#     print('Este numero é Positivo')
+# elif numero < 0:
+#     print('Este numero é Negativo')
+# else: 
+#     print('Zero')
+    
+#####
+
+#atv 3
+
+# numero = int(input('Digite um numero inteiro: '))
+
+# # numero = numero % 2
+
+# if numero % 2 == 0:
+#     print('Esse numero é par')
+# else: # numero == 1:
+#     print('Este numero é impar')
+
+#####
+
+#atv 4
+
+# media_aluno = (float(input("Qual sua média: ")))
+# #falta_aluno = (float(input("Quantas faltas vc tem? ")))
+
+# if media_aluno >= 7: #and falta_aluno <= 24:
+#     print(f'Passou de ano. A media foi {media_aluno}')
+# else: 
+#     print(f'Reprovou de ano. A media foi {media_aluno}')
+
+#####
+
+#atv 5
+
+# numero = int(input('Digite um numero inteiro: '))
+
+# numero2 = int(input('Digite outro numero inteiro: '))
+
+# if numero > numero2:
+#     print(f'O Numero {numero} é maior que o {numero2}')
+# elif numero < numero2:
+#     print(f'O Numero {numero2} é maior que o {numero}')
+# else:
+#     print('Os numeros são iguais')
+
+####
+
+#Nivel 2
+
+#Atv 6
+
+# idedeNadador = int(input('Digite sua idade: '))
+
+# if idedeNadador >= 5 and idedeNadador <= 7:
+#     print('Infantil A')
+    
+# elif idedeNadador >= 8 and idedeNadador <= 10:
+#     print('Infantil B')
+    
+# elif idedeNadador >= 11 and idedeNadador <= 13:
+#     print('Juvenil A')
+# elif idedeNadador >= 14 and idedeNadador <= 17:
+#     print('Juvenil B')
+# elif idedeNadador >= 18:
+#     print('Adulto')
+
+####
+
+#atv 7
+
+# peso = float(input('Digite seu peso: '))
+# altura = float(input('Digite sua altura: '))
+
+# imc = peso / (altura*altura) 
+
+# if imc < 18.5:
+#     print('Abaixo do peso')
+# elif imc >= 18.5 and imc <= 24.9:
+#     print('Peso ideal! Parabéns!!')
+# else:
+#     print('Acima do peso')
+
+#####
+
+#atv 8
+
+celcius = float(input('Digite a temperatura em Celsius: ')) 
+
+fahrenheit = bool(input('Você quer converter para Fahrenheit? Sim ou Não? ')).lower
+
+convertido
+
+if fahrenheit == true:
+    convertido = (celsius * 9/5) + 32
+    print(f' O grau {celsius} em Fahrenheit é {convertido}')
+else:
+    print(celsius)
+    
+
+    
+
+
+
+    
+
+
+#media_aluno = (float(input("Qual sua média: ")))
+#falta_aluno = (float(input("Quantas faltas vc tem? ")))
+
+#if media_aluno >= 6 and falta_aluno <= 24:
+#    print("Passou de ano")
+#    print(f'A media foi {media_aluno}')
+#else: 
+#    print("Reprovou de ano")
+#    print(f'A media foi: {media_aluno}')
+    
+#####
+
+# tipo_cliente = input("Digite o tipo: Comum, Gestante, Idoso: ").lower()
+    
+# if tipo_cliente == 'idoso' or tipo_cliente == 'gestante':
+#     print('Dirija-se para o Guichê Prioritario.')
+# elif tipo_cliente == 'comum':
+#     print('Dirija-se para o Guichê 1')
+
+#####
+
+# salario = float(input('Digite seu salario: '))
+
+# if salario <= 1000:
+#     print('Faixa 1')
+# elif salario <= 10000 and salario > 1000:
+#     print('Faixa 2')
+#     if salario > 4000:
+#         print('Nordeste')
+# else:
+#     print('Faixa 3')
+    
