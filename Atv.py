83ae8a2f6cceb8518379c25fe22436902ce40d38
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2026-04-07 22:09:45 -0300

    Comment out sections for exercises and validations
    Comment out various sections of code related to temperature conversion, product pricing, and user input validations.

--- Atv.py
+++ Atv.py
@@ -104,17 +104,190 @@
 
 #atv 8
 
-celcius = float(input('Digite a temperatura em Celsius: ')) 
+# celcius = float(input('Digite a temperatura em Celsius: ')) 
 
-fahrenheit = bool(input('Você quer converter para Fahrenheit? Sim ou Não? ')).lower
+# fahrenheit = bool(input('Você quer converter para Fahrenheit? Sim ou Não? ')).lower
 
-convertido
+# convertido
 
-if fahrenheit == true:
-    convertido = (celsius * 9/5) + 32
-    print(f' O grau {celsius} em Fahrenheit é {convertido}')
-else:
-    print(celsius)
+# if fahrenheit == true:
+#     convertido = (celsius * 9/5) + 32
+#     print(f' O grau {celsius} em Fahrenheit é {convertido}')
+# else:
+#     print(celsius)
+
+##########
+
+# Atv 9 
+
+# produto = int(input("Digite o valor de um produto: "))
+
+# if produto < 0:
+#     print(f'O produto tem um valor negativo')
+# elif produto <= 50:
+#     print(f'O valor do produto está Barato')
+# elif produto <= 100:
+#     print(f'O valor do produto está em um preço medio')
+# elif produto > 100:
+#     print(f'O valor do produto está Caro')
+
+#############
+
+# Atv 10
+
+# turno = input("Digite o Turno da sua aula, Usando M, V, N: ").lower()
+
+
+# if turno == 'm':
+#     print('Bom dia')
+# elif turno == 'v':
+#     print('Boa Tarde')
+# elif turno == 'n':
+#     print('Boa Noite')
+
+###########
+
+# Atv 11
+
+# login = input('Digite seu login: ')
+# senha = input('Digite sua senha: ')
+
+# if login == 'admin' and senha == 'fatec123':
+#     print('Bem vindo!!')
+# else:
+#     print('CAI FORA DAQUI!!!')
+
+#########
+
+# Atv 12
+
+######
+
+
+# EXERCÍCIOS COM LAÇO WHILE (Condições e Validações)
+# Foco: Repetição baseada em estados e interatividade.
+# dia 07/04/26
+ 
+# 1. Até o Zero: Peça números ao usuário repetidamente. O
+# programa só para quando ele digitar 0.
+
+# num = int(input('Digite um numero: '))
+
+# while num != 0:
+#     num = int(input('Digite outro numero até vc acertar o numero: '))
+# print('Fim do programa')
+    
+# 2. Validação de Nota: Peça uma nota entre 0 e 10. Enquanto o
+# valor for inválido, continue pedindo.
+
+# nota = float(input('Digite a nota: '))
+
+# while nota < 0 or nota > 10:
+#     nota = float(input('Digite um numero valido: '))
+#     print('Voce Digitou um numero valido')
+
+# print('Fim do programa')
+
+# 3. Senha de Acesso: Crie um sistema que peça uma senha e só
+# libere o acesso quando a senha "python123" for digitada.
+    
+# senha = input('Digite a senha: ').lower()
+
+# senhaCerta = "python123"
+
+# while senha != senhaCerta:
+#     senha = input('Digite a senha certa: ').lower()
+#     print('Bem vindo!')
+    
+# print('Fim do programa')
+
+# 4. Soma Acumulada: Peça números ao usuário e vá somando-os.
+# Pare quando a soma total ultrapassar 100.
+
+# num = int(input('Digite um numero: '))
+
+# while num < 100:
+#     num2 = int(input('Digite outro numero'))
+#     num = num + num2
+#     print('Voce excedeu o limite')
+
+# print('Fim do programa')
+
+# 5. Menu de Calculadora: Exiba um menu: [1] Somar [2] Subtrair
+# [0] Sair. O programa deve repetir até o usuário escolher 0
+
+# num = int(input('[1] Somar [2] Subtrair [0] Sair.\n'))
+
+# while num != 0:
+#     if num == 1:
+#         num1 = int(input('Digite o primeiro numero: '))
+#         num2 = int(input('Digite o segundo numero: '))
+#         resultadoSoma = num1 + num2
+#         print(resultadoSoma)
+#     elif num == 2:
+#         num1 = int(input('Digite o primeiro numero: '))
+#         num2 = int(input('Digite o segundo numero: '))
+#         resultadoSub = num1 - num2
+#         print(resultadoSub)
+#     else:
+#         print('Até mais...')
+#     num = int(input('\nGostei vamos de novo: [1] Somar [2] Subtrair [0] Sair.\n'))
+    
+# print('Fim do programa')
+
+# 6. Adivinhação Simples: Defina um número secreto. O usuário deve tentar
+# adivinhar; o programa avisa se o chute foi alto ou baixo até ele acertar.
+
+# print("================================")
+# print("Bem-vindo ao jogo de Adivinhação")
+# print("================================")
+
+
+# import random
+
+# numero = random.randint(0, 100)
+
+# print("")
+
+# tentativas = 10
+# rodada = 1
+
+# while (rodada <= tentativas):
+#     print("Tentativa {} de 10".format(rodada, tentativas))
+
+#     chute_str = input("Digite o seu numero: ")
+#     print("Você Digitou", chute_str)
+#     chute = int(chute_str)
+
+#     acertou = chute == numero
+#     maior = chute < numero
+#     menor = chute > numero
+
+#     if(acertou):
+#         print("Parabéns!! Você acertou ;)")
+#         rodada = rodada + 1
+#     else:
+#         if(maior):
+#             print("O número é maior que", chute)
+#         elif(menor):
+#             print("O numero é menor que", chute)
+
+#     rodada = rodada + 1
+
+# print("Fim de jogo")
+
+# 7. População de Bactérias: Uma colônia dobra a cada hora. Começando
+# com 1 bactéria, em quantas horas ela ultrapassará 1.000.000?
+
+bacterias = 1
+horas = 0
+
+while bacterias <= 1000000:
+    bacterias *= 2 #2 horas
+    horas += 1 #variavel horas adicina mais 1, e a cada 2h a variavel bactéria 
+    #multiplica por 2
+
+print(f'Ultrapassa 1.000.000 em {horas} horas')
     
 
     
